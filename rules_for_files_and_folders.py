7090788902648caedac3b7f67fdb4c64f638c4a7
JustinMNIC <104854769+JustinMNIC@users.noreply.github.com>
2024-03-15 23:53:25 +0200

    Update rules_for_files_and_folders.py

--- rules_for_files_and_folders.py
+++ rules_for_files_and_folders.py
@@ -7,81 +7,6 @@ from PIL import Image
 import time 
 
 
-class Executor_Rule():
-    def __check_criteria(self, file, criteria):
-        if criteria["name"] == "all" or file.name in criteria["name"]:
-            if criteria["type"] == "all" or file.type in criteria["type"]:
-                if criteria["size"] == "all" or ((criteria["size"]["above_or_below"] == "above" and file.size > criteria["size"]["size"])) or ((criteria["size"]["above_or_below"] == "below" and file.size < criteria["size"]["size"])):
-                    if criteria["date_modified"] == "all" or ((criteria["date_modified"]["before_or_after"] == "before" and file.date_modified < criteria["date_modified"]["date"])) or (criteria["date_modified"]["before_or_after"] == "after" and file.date_modified > criteria["date_modified"]["date"]):
-                        return True
-        return False
-    
-    def __check_exception(self, file, exception):
-        if exception["name"] == None or file.name not in exception["name"]:
-            if exception["type"] == None or file.type not in exception["type"]:
-                if exception["size"] == None or ((exception["size"]["above_or_below"] == "above" and file.size > exception["size"]["size"])) or ((exception["size"]["above_or_below"] == "below" and file.size < exception["size"]["size"])):
-                    if exception["date_modified"] == None or ((exception["date_modified"]["before_or_after"] == "before" and file.date_modified < exception["date_modified"]["date"])) or (exception["date_modified"]["before_or_after"] == "after" and file.date_modified > exception["date_modified"]["date"]):
-                        return True
-        return False
-        
-    def move_files(self, source, destination, criteria = None, exception = None):
-        for file in os.scandir(source):
-            if file.is_file():
-                if criteria != None:
-                    if self.__check_criteria(file, criteria):
-                        if exception != None:
-                            if self.__check_exception(file, exception):
-                                shutil.move(file, destination)
-                        else:
-                            shutil.move(file, destination)
-                else:
-                    if exception != None:
-                        if self.__check_exception(file, exception):
-                            shutil.move(file, destination)
-                    else:
-                        shutil.move(file, destination)
-            else:
-                self.move_files(file, destination, criteria, exception)
-                
-    def copy_files(self, source, destination, criteria = None, exception = None):
-        for file in os.scandir(source):
-            if file.is_file():
-                if criteria != None:
-                    if self.__check_criteria(file, criteria):
-                        if exception != None:
-                            if self.__check_exception(file, exception):
-                                shutil.copy(file, destination)
-                        else:
-                            shutil.copy(file, destination)
-                else:
-                    if exception != None:
-                        if self.__check_exception(file, exception):
-                            shutil.copy(file, destination)
-                    else:
-                        shutil.copy(file, destination)
-            else:
-                self.copy_files(file, destination, criteria, exception)
-                
-    def delete_files(self, source, criteria = None, exception = None):
-        for file in os.scandir(source):
-            if file.is_file():
-                if criteria != None:
-                    if self.__check_criteria(file, criteria):
-                        if exception != None:
-                            if self.__check_exception(file, exception):
-                                os.remove(file)
-                        else:
-                            os.remove(file)
-                else:
-                    if exception != None:
-                        if self.__check_exception(file, exception):
-                            os.remove(file)
-                    else:
-                        os.remove(file)
-            else:
-                self.delete_files(file, criteria, exception)
-
-
 ctk.set_default_color_theme("green")# Themes: "blue" (standard), "green", "dark-blue"
 ctk.set_appearance_mode("dark")# Modes: "system" (standard), "light", "dark"
 
@@ -158,6 +83,12 @@ class GUI_Rules_for_files_and_folders(ctk.CTk):
             ### functions to run at the start 
             self.display_rules()
             self.display_panda()
+            self.start()
+            
+    def start(self):
+        Executor.start(Executor)
+        if Executor.running == False:
+            self.after(30000, self.start)
     
     def display_panda(self):
         self.label_sleeping_panda = ctk.CTkLabel(master = self, image= self.sleeping_panda, text="")
@@ -181,13 +112,18 @@ class GUI_Rules_for_files_and_folders(ctk.CTk):
         else:
             self.count_panda += 1
     
-    def pre_check_before_saving_the_rule(self):        
+    def pre_check_before_saving_the_rule(self):       
+        with open("rules_for_files_and_folders.json", "r") as file:
+            data_json = json.load(file)
+            
         if self.json_new_rule["action"] == None:
             messagebox.showerror("Error", "You must select an action for your rule")
         elif self.json_new_rule["action"] == "Move" and self.json_new_rule["where_to_move"] == None:
             messagebox.showerror("Error", "You must select a destination for your files")
         elif self.json_new_rule["action"] == "Copy" and self.json_new_rule["where_to_copy"] == None:
             messagebox.showerror("Error", "You must select a destination for your files")
+        elif self.entry_box_give_nikname.get() in data_json.keys():
+            messagebox.showerror("Error", "You already have a rule with that name\n\nPlease select another name for your rule")
         else:
             self.func_create_top_level_and_ask_for_confirmation_to_save_the_rule()
             
@@ -231,33 +167,68 @@ class GUI_Rules_for_files_and_folders(ctk.CTk):
         
         with open("rules_for_files_and_folders.json", "w") as file:
             json.dump(data_json, file, indent= 4)
+         
         self.reset_vars_and_widgets()
-        #TODO #self.display_rules() this should be updated so it doesn't cause a visual bug + test class Rules to see if it works properly
-       
+        self.display_rules()
+        
+    
     def display_rules(self):
-        try:
-            with open("rules_for_files_and_folders.json", "r") as file:
-                rules = json.load(file)
-            if rules != {}:
-                for rule in rules:
-                    Rules(master = self.scrollable_frame_for_my_rules, nikname = rule, rule_id = rules[rule])  #TODO check if this works properly
-            else:
-                ctk.CTkLabel(master = self.scrollable_frame_for_my_rules, text = "No rules found", font = ("Consolas", 18)).pack()
-        except:
+        if not os.path.exists("rules_for_files_and_folders.json"):
             with open("rules_for_files_and_folders.json", "w") as file:
                 json.dump({}, file)
             ctk.CTkLabel(master = self.scrollable_frame_for_my_rules, text = "Welcome!\nClick on 'New Rule'\nto start", font = ("Consolas", 18)).pack()
+        elif os.path.exists("rules_for_files_and_folders.json"):
+            #if the json file is empty ({}) then display "No rules found"
+            with open("rules_for_files_and_folders.json", "r") as file:
+                rules = json.load(file)
+            if rules == {}:
+                ctk.CTkLabel(master = self.scrollable_frame_for_my_rules, text = "No rules found", font = ("Consolas", 18)).pack()
+            else:
+                for widget in self.scrollable_frame_for_my_rules.winfo_children():
+                    widget.destroy()
+                for rule_name, rule_details in rules.items():
+                    self.rule = ctk.CTkFrame(master = self.scrollable_frame_for_my_rules, width = 300, height = 50, border_width = 1)
+                    self.rule.pack(side = "top", fill = "x")
+                    self.rule_name = ctk.CTkLabel(master = self.rule, text = rule_name, font = ("Consolas", 18, "bold"), text_color= "lightblue")
+                    self.rule_name.pack(side = "left")
+                    self.button_delete_rule = ctk.CTkButton(master = self.rule,
+                                                       text = "Delete",
+                                                       font = ("Consolas", 18),
+                                                       hover_color= "darkred",
+                                                       command = lambda: self.rule_deleted(rule_name))
+                    self.button_delete_rule.pack(side = "right")
     
+    def rule_deleted(self, rule_name):
+        #remove rule_name from the json file
+        with open("rules_for_files_and_folders.json", "r") as file:
+            data_json = json.load(file)
+        data_json.pop(rule_name)
+        with open("rules_for_files_and_folders.json", "w") as file:
+            json.dump(data_json, file, indent= 4)
+
+        #remove all rules from the scrollable_frame_for_my_rules
+        for widget in self.scrollable_frame_for_my_rules.winfo_children():
+            widget.destroy()
+            
+        self.display_rules()
+        
     def reset_vars_and_widgets(self):
+        self.current_directory = ""
         self.count_panda = 0
+        
         self.cash_criteria_name = []
         self.cash_criteria_type = []
         self.cash_criteria_size = {}
+        
         self.cash_exception_name = []
         self.cash_exception_type = []
         self.cash_exception_size = {}
+        
         self.cash_criterias_selected = ""
         self.cash_exceptions_selected = ""
+        
+        self.button_save_rule.place_forget()
+        self.entry_box_give_nikname.place_forget()
         
         for widget in self.winfo_children():
             if widget not in [self.frame_display_rules, self.frame_for_buttons]:
@@ -592,27 +563,164 @@ class GUI_Rules_for_files_and_folders(ctk.CTk):
                                                                     wraplength= 600)
         self.label_show_where_the_file_will_be_copied.place(x = 460, y = 210)
     
-class Rules(ctk.CTk):    
-    def __init__(self, *args, nikname, rule_id, **kwargs):
-            super().__init__(*args, nikname, rule_id, **kwargs)
-            self.nikname = nikname
-            self.rule_id = rule_id
-            self.frame_rule = ctk.CTkFrame(master = self)
-            self.frame_rule.pack(side = "top", fill = "x")
-            self.title = ctk.CTkLabel(master = self.frame_rule, text = self.nikname if self.nikname != None else "My Rule", font = ("Consolas", 18), wraplength= 250)
-            self.title.pack(side = "left", fill = "x")
-            self.delte_rule = ctk.CTkButton(master = self.frame_rule, text = "Del.", font = ("Consolas", 18), command = self.delete_rule)
-            self.delte_rule.pack(side = "right", fill = "x")
-            
-    def delete_rule(self):
+    def path_doesnt_exist(self, rule_name, path):
+        messagebox.showerror("Error", f"The path: {path} doesn't exist\n\nPlease update the path for the rule: {rule_name}")
+        #chanege the name of the rule to Error: Path
         with open("rules_for_files_and_folders.json", "r") as file:
-            rules = json.load(file)
-        del rules[self.nikname]
+            data_json = json.load(file)
+        data_json["Error: Path"] = data_json.pop(rule_name)
         with open("rules_for_files_and_folders.json", "w") as file:
-            json.dump(rules, file)
-        self.destroy()
+            json.dump(data_json, file, indent= 4)
+        self.display_rules()
         
-
+        
+class Executor():
+    running = False
+    def start(self):
+        self.running = True
+        
+        with open("rules_for_files_and_folders.json", "r") as file:
+            data_json = json.load(file)
+            
+        for name, data in data_json.items():
+            
+            self.check_if_path_still_exists(self, data["target_directory"], name)
+            
+            if data["action"] == "Move":
+                self.check_if_path_still_exists(self, data["where_to_move"], name)
+                self.move_files(self = self, data = data)
+            elif data["action"] == "Copy":
+                self.check_if_path_still_exists(self, data["where_to_copy"], name)
+                self.copy_files(self = self, data = data)
+            elif data["action"] == "Delete":
+                self.delete_files(self, data)
+            
+        self.running = False
+              
+    def move_files(self, data):
+        for root, dirs, files in os.walk(data["target_directory"]):
+            for file in files:
+                if self.check_criteria(self, data, file) and self.check_exceptions(self, data, file):
+                    shutil.move(os.path.join(root, file), data["where_to_move"])
+            for dir in dirs:
+                if self.check_criteria(self, data, dir) and self.check_exceptions(self, data, dir):
+                    shutil.move(os.path.join(root, dir), data["where_to_move"])
+                    
+    def copy_files(self, data):
+        for root, dirs, files in os.walk(data["target_directory"]):
+            for file in files:
+                if self.check_criteria(self, data, file) and self.check_exceptions(self, data, file):
+                    shutil.copy(os.path.join(root, file), data["where_to_copy"])
+            for dir in dirs:
+                if self.check_criteria(self, data, dir) and self.check_exceptions(self, data, dir):
+                    shutil.copy(os.path.join(root, dir), data["where_to_copy"])
+                    
+    def delete_files(self, data):
+        for root, dirs, files in os.walk(data["target_directory"]):
+            for file in files:
+                if self.check_criteria(self, data, file) and self.check_exceptions(self, data, file):
+                    os.remove(os.path.join(root, file))
+            for dir in dirs:
+                if self.check_criteria(self, data, dir) and self.check_exceptions(self, data, dir):
+                    shutil.rmtree(os.path.join(root, dir))
+    
+    def check_criteria(self, data, file):
+        if "." in file:
+            cash = file.split(".")
+            file_name, file_type = cash[0], cash[1]
+        else:
+            file_name, file_type = file, "folder"
+        
+        C_size = False
+        C_name = False
+        C_type = False
+        
+        #criteria name
+        if data["criteria"]["name"] != None:
+            for name in data["criteria"]["name"]:
+                if name in file_name:
+                    C_name = True
+        else:
+            C_name = True
+        
+        #criteria type
+        if data["criteria"]["type"] != None:
+            for variants in ["dir", "folder", "directory"]:
+                if variants in data["criteria"]["type"]:
+                    if os.path.isdir(file):
+                        C_type = True
+            for typee in data["criteria"]["type"]:
+                if typee in file_type:
+                    C_type = True
+        else:
+            C_type = True
+        
+        #criteria size
+        if data["criteria"]["size"] != None:
+            if data["criteria"]["size"]["above_or_below"] == "above":
+                if os.path.getsize(file) > data["criteria"]["size"]["size"]:
+                    C_size = True
+                else:
+                    C_size = False
+            elif data["criteria"]["size"]["above_or_below"] == "below":
+                if os.path.getsize(file) < data["criteria"]["size"]["size"]:
+                    C_size = True
+                else:
+                    C_size = False
+        else:
+            C_size = True
+        return True if C_name and C_type and C_size else False
+    
+    def check_exceptions(self, data, file):
+        if "." in file:
+            cash = file.split(".")
+            file_name, file_type = cash[0], cash[1]
+        else:
+            file_name, file_type = file, "folder"
+        
+        E_size = False
+        E_name = False
+        E_type = False
+        
+        if data["exception"]["name"] != None:
+            for name in data["exception"]["name"]:
+                if name in file_name:
+                    E_name = True
+        else:
+            E_name = True
+            
+        if data["exception"]["type"] != None:
+            for variants in ["dir", "folder", "directory"]:
+                if variants in data["exception"]["type"]:
+                    if os.path.isdir(file):
+                        E_type = True
+            for typee in data["exception"]["type"]:
+                if typee in file_type:
+                    E_type = True
+        else:
+            E_type = True
+            
+        if data["exception"]["size"] != None:
+            if data["exception"]["size"]["above_or_below"] == "above":
+                if os.path.getsize(file) > data["exception"]["size"]["size"]:
+                    E_size = True
+                else:
+                    E_size = False
+            elif data["exception"]["size"]["above_or_below"] == "below":
+                if os.path.getsize(file) < data["exception"]["size"]["size"]:
+                    E_size = True
+                else:
+                    E_size = False
+        else:
+            E_size = True
+        return False if E_name and E_type and E_size else True
+    
+    def check_if_path_still_exists(self, path, rule_name):
+        if os.path.exists(path):
+            return True
+        else:
+            GUI_Rules_for_files_and_folders.path_doesnt_exist(path, rule_name)
+            
 if __name__ == "__main__":
     app = GUI_Rules_for_files_and_folders()
     app.mainloop()
